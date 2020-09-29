2e75e255afebda54b5acd109cd560ba28b01022f
German322VDK <germean322@gmail.com>
2020-09-29 23:39:15 +1000

    Основые модели

---
+++ Project send Email/Models/Recipient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_send_Email.Models
+{
+    class Recipient
+    {
+        public string Name { set; get; }
+        public string Address { set; get; }
+    }
+}
---
+++ Project send Email/Models/Servers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_send_Email.Models
+{
+    class Servers
+    {
+        public string Address { get; set; }
+        private int _Port;
+
+        public int Port 
+        {
+            get => _Port; 
+            set
+            {
+                if (value < 0 || value > 65535)
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "Номер порта должен быть в диапозоне");
+                _Port = value;
+            }
+        }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public bool UserSsl { get; set; }
+        public string Description { get; set; }
+
+    }
+}
