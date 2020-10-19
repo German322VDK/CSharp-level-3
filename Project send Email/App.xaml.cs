1369031b66e7519715975dcd41e2d9fe7026cdc2
German322VDK <germean322@gmail.com>
2020-10-19 20:19:14 +1000

    Завершение

--- Project send Email/App.xaml.cs
+++ Project send Email/App.xaml.cs
@@ -6,6 +6,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Project_send_Email.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Windows;
+using System.Linq;
+using Project_send_Email.lib.Models;
+using Project_send_Email.Data.Stores.InDB;
 
 namespace Project_send_Email
 {
@@ -18,6 +24,10 @@ namespace Project_send_Email
 
         public static IHost Hosting => _Hosting
             ??= Host.CreateDefaultBuilder(Environment.GetCommandLineArgs())
+            .ConfigureHostConfiguration(cfg => cfg
+                   .AddJsonFile("appconfig.json", true, true)
+                   .AddXmlFile("appsettings.xml", true, true)
+                )
             .ConfigureAppConfiguration(cfg => cfg
                    .AddJsonFile("appconfig.json", true, true)
                    .AddXmlFile("appsettings.xml", true, true)
@@ -41,13 +51,31 @@ namespace Project_send_Email
             services.AddTransient<IMailService, SmtpMailService>();
 #endif
             services.AddSingleton<IEncryptorService, Rfc2898Encryptor>();
+            services.AddDbContext<Project_send_EmailDB>(opt => opt
+               .UseSqlServer(host.Configuration.GetConnectionString("Default")));
+            services.AddTransient<Project_send_EmailDbInitializer>();
             // services.AddScoped<>()
-
+            services.AddSingleton<IStore<Recipient>, RecipientsStoreInDB>();
             //using (var scope = Services.CreateScope())
             //{
             //    var mail_service = scope.ServiceProvider.GetRequiredService<IMailService>();
             //    var sender = mail_service.GetSender("smtp.mail.ru", 25, true, "Login", "Password");
             //    sender.Send("sender@mail.ru", "recipient@gmail.com", "Title", "Body");
+            //}
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Services.GetRequiredService<Project_send_EmailDbInitializer>().Initialize();
+            base.OnStartup(e);
+            //using (var db = Services.GetRequiredService<Project_send_EmailDB>())
+            //{
+            //    var to_remove = db.SchedulerTasks.Where(task => task.Time < DateTime.Now);
+            //    if (to_remove.Any())
+            //    {
+            //        db.SchedulerTasks.RemoveRange(to_remove);
+            //        db.SaveChanges();
+            //    }
             //}
         }
     }
---
+++ Project send Email/Data/Project_send_EmailDbInitializer.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_send_Email.Data
+{
+    class Project_send_EmailDbInitializer
+    {
+        private readonly Project_send_EmailDB _db;
+
+        public Project_send_EmailDbInitializer(Project_send_EmailDB db) => _db = db;
+
+        public void Initialize()
+        {
+            _db.Database.Migrate();
+
+            InitializeRecipients();
+            InitializeSenders();
+            InitializeServers();
+            InitializeMessages();
+        }
+
+        private void InitializeRecipients()
+        {
+            if (_db.Recipients.Any()) return;
+
+            _db.Recipients.AddRange(TestData.Recipients);
+            _db.SaveChanges();
+        }
+
+        private void InitializeSenders()
+        {
+            if (_db.Senders.Any()) return;
+
+            _db.Senders.AddRange(TestData.Senders);
+            _db.SaveChanges();
+        }
+
+        private void InitializeMessages()
+        {
+            if (_db.Messages.Any()) return;
+
+            _db.Messages.AddRange(TestData.Messages);
+            _db.SaveChanges();
+        }
+
+        private void InitializeServers()
+        {
+            if (_db.Servers.Any()) return;
+
+            _db.Servers.AddRange(TestData.Serverss);
+            _db.SaveChanges();
+        }
+    }
+}
